bc3f41fda9110bec611925687715148314abed72
Christian.B <Christian.Buaka@hotmail.com>
2024-01-19 21:07:27 +0100

    👽️ Added a displayCharacters function

--- Dragonball-API/src/main.ts
+++ Dragonball-API/src/main.ts
@@ -1,16 +1,35 @@
 import axios from "axios";
 
+interface dbzCharacter {
+    name: string;
+    image: string;
+    ki: number;
+    gender: string;
+    race: string;
+}
+
 const getDBZ = async () => {
     try {
-    const response = await axios.get('https://dragonball-api.com/api/characters?page=1&limit=4&gender=Male&race=Saiyan');
+    const response = await axios.get<dbzCharacter[]>('https://dragonball-api.com/api/characters?page=1&limit=4&gender=Male&race=Saiyan');
     const dbzData = response.data;
     console.log('API Response:', dbzData);
-
+    displayCharacters(dbzData);
 
     } catch (error) {
         console.error('Could not get Dragon Ball Z-data:', error);
     }
     
-}
+};
+
+const displayCharacters = (characters: dbzCharacter[]) => {
+    const dbzContainer = document.getElementById('dbzContainer') as HTMLElement;
+
+    characters.forEach(character =>{
+        const characterElement = document.createElement('div');
+        characterElement.innerHTML = `<h2>${character.name}</h2> <img src="${character.image}" alt="${character.name}" class="dbzCharacters">`;
+
+        dbzContainer.appendChild(characterElement);
+    });
+};
 
 getDBZ()
